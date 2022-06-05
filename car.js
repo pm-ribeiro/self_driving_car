984effcd723a75c442ff8ccea65f42389c10ecc2
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-06-05 17:41:10 -0300

    refactor: separate moving logic to its own method

--- car.js
+++ car.js
@@ -14,7 +14,7 @@ class Car{
       this.controls=new Controls();
   }
 
-  update() {
+  #move() {
     if(this.controls.forward){
       this.speed += this.acceleration;
     }
@@ -60,6 +60,10 @@ class Car{
 
     this.x -= Math.sin(this.angle) * this.speed;
     this.y -= Math.cos(this.angle) * this.speed;
+  }
+
+  update() {
+    this.#move();
   }
 
   draw(ctx){
