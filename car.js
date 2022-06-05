e41c6bf2a5421a77b0cab8558931f38d33b6ea33
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-06-05 17:32:53 -0300

    feat: added rotation

--- car.js
+++ car.js
@@ -56,13 +56,19 @@ class Car{
   }
 
   draw(ctx){
-      ctx.beginPath();
-      ctx.rect(
-        this.x - this.width/2,
-        this.y - this.height/2,
-        this.width,
-        this.height
-      );
-      ctx.fill();
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+
+    ctx.beginPath();
+    ctx.rect(
+      - this.width/2,
+      - this.height/2,
+      this.width,
+      this.height
+    );
+    ctx.fill();
+
+    ctx.restore();
   }
 }
