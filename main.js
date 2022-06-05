670ff0a9aa1087e9e0bb631b7b5efdede3ab5fe7
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-06-05 17:48:27 -0300

    feat: added road logics

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ canvas.width = 200;
 
 // get canvas context to draw in the canvas
 const ctx = canvas.getContext("2d");
+const road=new Road(canvas.width/2, canvas.width);
 const car=new Car(100,100,30,50);
 
 animate();
@@ -11,6 +12,7 @@ animate();
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
+  road.draw(ctx);
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
--- road.js
+++ road.js
@@ -1,5 +1,33 @@
 class Road{
-  constructor(){
+  constructor(x, width, laneCount = 3){
+    this.x = x;
+    this.width = width;
+    this.laneCount = laneCount;
 
+    this.left = x - width/2;
+    this.right = x + width/2;
+
+    const infinity = 1000000;
+
+    //!!IMPORTANT: Y in a computer grows downwards
+    this.top = -infinity;
+    this.bottom = infinity;
+  }
+
+  draw(ctx){
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "white";
+
+    // drwa line to the left side of the road
+    ctx.beginPath();
+    ctx.moveTo(this.left, this.top);
+    ctx.lineTo(this.left, this.bottom);
+    ctx.stroke();
+
+    // drwa line to the right side of the road
+    ctx.beginPath();
+    ctx.moveTo(this.right, this.top);
+    ctx.lineTo(this.right, this.bottom);
+    ctx.stroke();
   }
 }
