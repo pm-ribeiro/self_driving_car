43fea651c6f4ebb95d8960cd1bf8015fa3d42650
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-06-23 17:18:36 -0300

    refactor: update road

--- road.js
+++ road.js
@@ -1,5 +1,3 @@
-
-
 class Road{
   constructor(x, width, laneCount = 3){
     this.x = x;
@@ -19,32 +17,31 @@ class Road{
     const topRight = {x: this.right, y: this.top};
     const bottomLeft = {x: this.left, y: this.bottom};
     const bottomRight = {x: this.right, y: this.bottom};
-
     this.borders = [
       [topLeft, bottomLeft],
       [topRight, bottomRight]
-    ]
+    ];
   }
 
-  getLaneCenter(laneIndex) {
+  getLaneCenter(laneIndex){
     const laneWidth = this.width / this.laneCount;
     return this.left + laneWidth/2 + Math.min(
-      laneIndex, this.laneCount - 1
-    )*laneWidth;
+        laneIndex, this.laneCount - 1
+      )*laneWidth;
   }
 
   draw(ctx){
     ctx.lineWidth = 5;
     ctx.strokeStyle = "white";
 
-    for(let i = 1; i <= this.laneCount - 1; i++){
+    for(let i = 1; i<=this.laneCount-1; i++){
       const x = lerp(
         this.left,
         this.right,
         i/this.laneCount
       );
+
       ctx.setLineDash([20,20]);
-      // draw lines
       ctx.beginPath();
       ctx.moveTo(x, this.top);
       ctx.lineTo(x, this.bottom);
@@ -52,13 +49,11 @@ class Road{
     }
 
     ctx.setLineDash([]);
-
     this.borders.forEach(border => {
       ctx.beginPath();
       ctx.moveTo(border[0].x, border[0].y);
       ctx.lineTo(border[1].x, border[1].y);
       ctx.stroke();
-    })
-
+    });
   }
 }
