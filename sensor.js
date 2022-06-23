18eba16401bf9f4b26bd18287a9fa05ba9d54b31
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-06-23 17:24:44 -0300

    refactor: updated sensors according to github due to bug

--- sensor.js
+++ sensor.js
@@ -1,60 +1,55 @@
-class Sensor {
-  constructor(car) {
+class Sensor{
+  constructor(car){
     this.car = car;
     this.rayCount = 3;
     this.rayLength = 100;
     this.raySpread = Math.PI/4; // 45 degrees
 
     this.rays = [];
-
     this.readings = [];
   }
 
-  update(roadBorders) {
+  update(roadBorders){
     this.#castRays();
     this.readings = [];
-    for (let i = 0; i < this.rays; i++) {
+    for(let i = 0; i<this.rays.length; i++){
       this.readings.push(
-        this.#getReading(this.rays[i], roadBorders)
+          this.#getReading(this.rays[i], roadBorders)
       );
     }
   }
 
-  #getReading(ray, roadBorders) {
-    let touches = [];
-    for (let i = 0; i < roadBorders; i++) {
-      const touch = getIntersection(
-        ray[0], ray[1],
-        roadBorders[i][0],
-        roadBorders[i][1]
-      )
+  #getReading(ray,roadBorders){
+      let touches = [];
 
-      if(touch) {
-        touches.push(touch);
+      for(let i = 0; i < roadBorders.length; i++){
+        const touch = getIntersection(
+          ray[0],
+          ray[1],
+          roadBorders[i][0],
+          roadBorders[i][1]
+        );
+        if(touch){
+          touches.push(touch);
+        }
       }
 
-    }
-
-    if(touches.length === 0) {
-      return null;
-    } else {
-      const offsets = touches.map(
-        e => e.offset
-      );
-
-      const minOffset = Math.min(...offsets);
-
-      return touches.find(e => e.offset === minOffset);
-    }
+      if(touches.length === 0){
+        return null;
+      } else {
+        const offsets = touches.map(e => e.offset);
+        const minOffset = Math.min(...offsets);
+        return touches.find(e => e.offset == minOffset);
+      }
   }
 
-  #castRays() {
+  #castRays(){
     this.rays = [];
-    for (let i = 0; i < this.rayCount; i++) {
+    for(let i = 0; i < this.rayCount; i++){
       const rayAngle = lerp(
-        this.raySpread / 2,
-        -this.raySpread / 2,
-        this.rayCount === 1 ? 0.5 : i/(this.rayCount - 1)
+        this.raySpread/2,
+        -this.raySpread/2,
+        this.rayCount == 1 ? 0.5 : i/(this.rayCount-1)
       )+this.car.angle;
 
       const start = {
@@ -63,21 +58,17 @@ class Sensor {
       };
 
       const end = {
-        x: this.car.x - Math.sin(rayAngle) * this.rayLength ,
-        y: this.car.y - Math.cos(rayAngle) * this.rayLength ,
+        x: this.car.x - Math.sin(rayAngle) * this.rayLength,
+        y: this.car.y - Math.cos(rayAngle) * this.rayLength
       };
-
-      this.rays.push([
-        start, end
-      ]);
+      this.rays.push([start, end]);
     }
   }
 
-  draw(ctx) {
-    for (let i = 0; i < this.rayCount; i++) {
+  draw(ctx){
+    for(let i = 0; i < this.rayCount; i++){
       let end = this.rays[i][1];
-
-      if(this.readings[i]) {
+      if(this.readings[i]){
         end = this.readings[i];
       }
 
@@ -86,14 +77,12 @@ class Sensor {
       ctx.strokeStyle = "yellow";
       ctx.moveTo(
         this.rays[i][0].x,
-        this.rays[i][0].y,
+        this.rays[i][0].y
       );
-
       ctx.lineTo(
         end.x,
         end.y
       );
-
       ctx.stroke();
 
       ctx.beginPath();
@@ -101,15 +90,13 @@ class Sensor {
       ctx.strokeStyle = "red";
       ctx.moveTo(
         this.rays[i][1].x,
-        this.rays[i][1].y,
+        this.rays[i][1].y
       );
-
       ctx.lineTo(
         end.x,
         end.y
       );
-
       ctx.stroke();
     }
-  }
+  }        
 }
