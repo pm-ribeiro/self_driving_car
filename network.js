e74dff7bebef5bc847e2b615ac78ade3a2bd774e
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-06-24 18:07:33 -0300

    feat: added NeuralNetwork

--- network.js
+++ network.js
@@ -1,3 +1,25 @@
+class NeuralNetwork {
+  constructor(neuronCount) {
+    this.levels = [];
+    for (let i = 0; i < neuronCount.length - 1; i++) {
+      this.levels.push(
+        new Level(neuronCount[i], neuronCount[i+1])
+      )
+    }
+  }
+
+  static feedForward(givenInputs, network) {
+    let outputs = Level.feedForward(
+      givenInputs, network.levels[0]
+    )
+
+    for (let i = 1; i < network.levels.length; i++) {
+      outputs = Level.feedForward(outputs, network.levels[i])
+    }
+
+    return outputs;
+  }
+}
 class Level {
   constructor(inputCount, outputCount) {
     this.inputs = new Array(inputCount);
