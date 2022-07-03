741ce9b4f0487ec8b25eaf2d2d35c9820b899896
pm-ribeiro <patricia.mr3.14@gmail.com>
2022-07-03 12:22:02 -0300

    feat: added U checking

--- segment_intersection/utils.js
+++ segment_intersection/utils.js
@@ -32,12 +32,14 @@ function getIntersection(A, B, C, D) {
  * t = top / bottom
  */
 
-  const top = (D.x - C.x)*(A.y - C.y) - (D.y - C.y)*(A.x - C.x);
+  const tTop = (D.x - C.x)*(A.y - C.y) - (D.y - C.y)*(A.x - C.x);
+  const uTop = (C.y-A.y)*(A.x-B.x)-(C.x-A.x)*(A.y-B.y);
   const bottom = (D.y - C.y)*(B.x - A.x) - (D.x - C.x)*(B.y - A.y)
 
   if(bottom != 0) {
-    const t = top / bottom;
-    if(t>= 0 && t<=1) {
+    const t = tTop / bottom;
+    const u = uTop / bottom;
+    if(t>= 0 && t<=1 && u>=0 && u<=1) {
       return {
         x: lerp(A.x, B.x, t),
         y: lerp(A.y, B.y, t),
